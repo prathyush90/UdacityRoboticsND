62a77e7825e51fb20eb47be4589e763a868b43f3
Prathyush <prathyush.manoharan1@gmail.com>
2019-03-28 21:10:50 +0530

    min

---
+++ Project_2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_2/src
+# Build directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(pet_robot)
---
+++ Project_2/build/pet_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_2/src/pet_robot
+# Build directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_2/build/pet_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
