780434a5612be11cf1e65ce07bc349594f6d8da6
prathyush <prathyush.manoharan1@gmail.com>
2019-06-13 19:21:18 +0530

    min

---
+++ Project_4/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_4/src
+# Build directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(pet_robot)
+subdirs(teleop_twist_keyboard)
+subdirs(pgm_map_creator)
---
+++ Project_4/build/pet_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_4/src/pet_robot
+# Build directory: /home/noob/Documents/UdacityRoboticsNanoDegree/Project_4/build/pet_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
